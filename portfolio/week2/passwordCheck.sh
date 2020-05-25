cb59c7b14896d6ee93eda10653cc1d14d669397b
Philip Higgs <phiggs@our.ecu.edu.au>
2020-05-25 23:58:44 +0800

    update

--- portfolio/week2/passwordCheck.sh
+++ portfolio/week2/passwordCheck.sh
@@ -8,6 +8,8 @@ read -s -p "Password: " password
 # Will be comparing the hashes of the files not the hashes inside the files
 # HASH of entered password goes into file
 echo "$password" | sha256sum > /home/phiggs/student/scripts/portfolio/week2/secretpw/secretpw2.txt
+cp /home/phiggs/student/scripts/portfolio/week2/secretpw/secretpw2.txt /home/phiggs/student/scripts/portfolio/week2/secretpw2.txt
+
 
 # The file with the hashed password has its checksum copied to a file
 # echo sha256sum /home/phiggs/student/scripts/portfolio/week2/secretpw/secretpw.txt  /home/phiggs/student/scripts/portfolio/week2/secretpw/secretpw.txt > /home/phiggs/student/scripts/portfolio/week2/secretpw/sha256.txt
@@ -19,4 +21,17 @@ echo "$password" | sha256sum > /home/phiggs/student/scripts/portfolio/week2/secr
 #echo -n cat ./secretpw/sha256.txt | sha256sum -c  ./secretpw/secretpw2.txt
 #echo "14bb4a88ca1378437689f64db055cd78488f4c01a103a3efc04bcbd639922b1b  ./secretpw/secretpw2.txt" | sha256sum --check
 
-echo -n "$(cat ./secretpw/sha.txt)" | sha256sum -c
+# echo -n "$(cat ./secretpw/sha2.txt)" | sha256sum -c
+
+# https://stackoverflow.com/questions/51546097/verify-sha-256-sum-of-a-file-and-exit-the-script-if-not-as-intended
+if ! echo -n "$(cat ./secretpw/sha2.txt)" | sha256sum -c -; then
+    echo
+    echo "Access Denied" >&2
+    echo
+    exit 1
+else
+    echo
+    echo "Access Granted"
+    echo
+fi
+exit 0
