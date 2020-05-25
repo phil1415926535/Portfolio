61566e16d30245b0b29c2b915fa1c3eb1ada25ca
Philip Higgs <phiggs@our.ecu.edu.au>
2020-05-25 23:14:41 +0800

    update

--- portfolio/week2/passwordCheck.sh
+++ portfolio/week2/passwordCheck.sh
@@ -15,5 +15,8 @@ echo "$password" | sha256sum > /home/phiggs/student/scripts/portfolio/week2/secr
 # Checksum of file of password compared to checksum in sha256 (which is of the secretpw.txt file)
 # echo -n "$(cat secretpw/sha256.txt)" | sha256sum --check secretpw/secretpw.txt
 
+#https://superuser.com/questions/1312740/how-to-take-sha256sum-of-file-and-compare-to-check-in-one-line
 #echo -n cat ./secretpw/sha256.txt | sha256sum -c  ./secretpw/secretpw2.txt
-echo "$password" | sha256sum | sha256sum -c ./secretpw/secretpw2.txt 
+#echo "14bb4a88ca1378437689f64db055cd78488f4c01a103a3efc04bcbd639922b1b  ./secretpw/secretpw2.txt" | sha256sum --check
+
+echo -n "cat sha2.txt" | sha256sum -c  secretpw2.txt
