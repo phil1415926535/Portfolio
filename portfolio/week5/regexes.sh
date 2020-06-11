1b81b4236485904da7a657d8f4d1b4aafdf6f1e9
Philip Higgs <phiggs@our.ecu.edu.au>
2020-06-11 16:28:42 +0800

    updated work

--- portfolio/week5/regexes.sh
+++ portfolio/week5/regexes.sh
@@ -78,10 +78,11 @@ case $option in
         read -p "Pressing any key: back to menu"
     ;;
     4 )
-        echo "Command: grep -wrEnI "^echo\s|[[:blank:]]{1,}echo\s" ~/student/scripts/portfolio --exclude-dir=.git --color"
+        echo "Command: grep -wrEnI "^echo\s|[[:blank:]]{1,}echo\s" ~/student/scripts/portfolio --exclude-dir=.git --color | grep -E "[^echo]\".(\w+\s){3,}"
         echo
         echo "Output:"
-        grep -wrEnI "^echo\s|[[:blank:]]{1,}echo\s" ~/student/scripts/portfolio --exclude-dir=.git --color # finds all echo statements in files
+        grep -wrEnI "^echo\s|[[:blank:]]{1,}echo\s" ~/student/scripts/portfolio --exclude-dir=.git --color | grep -E "[^echo]\".(\w+\s){3,}" # finds all echo statements in files
+        grep -wrEnI "^echo\s.*(\w+\s){3,} | [[:blank:]]{1,}echo\s.*(\w+\s){3,}" ~/student/scripts/portfolio
         read -p "Pressing any key: back to menu"
     ;;
     5a )
---
+++ portfolio/week6/test/ruby.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+character_name = "John"
+character_age = "35"
+
+puts ("There one was a man named " + character_name)
+puts ("he was " + character_age + " years old")
+puts ("he really liked the name " + character_name)
+puts ("but didn't like being " +character_age)
